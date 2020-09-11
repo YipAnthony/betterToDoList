20df31952edba4da5179a0f1eda1df7bb4b317f6
Anthony Yip <anthonyjtyip@gmail.com>
2020-09-11 09:37:57 -0700

    add basic html/css structure to visualize project no major JS yet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('hi hi')
+let toDoList = (() => {
+
+    const taskFactoryFunc = (description, dueDate, project, priority, completion) => {
+        // const completion = "",
+        return {description, dueDate, project, priority,completion};
+    }
+    let task = taskFactoryFunc("testestest", "1/1/2020", "projecttest", "high","")
+
+    const taskArray = [];
+    
+    const pushTaskObjtoArray = (createdTask) => {
+        taskArray.push(createdTask)
+    }
+
+    //delete task from array
+
+
+
+    console.log(taskArray)
+
+    return {taskFactoryFunc, taskArray}
+})();
