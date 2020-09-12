566c484369f411ca06a64f71270f4c4d6925002a
Anthony Yip <anthonyjtyip@gmail.com>
2020-09-12 10:57:12 -0700

    Added toggle function for task icon/form Refactor methods into class specific functions

--- src/index.js
+++ src/index.js
@@ -12,11 +12,8 @@ let toDoList = (() => {
         taskArray.push(createdTask)
     }
 
-    //delete task from array
 
-    const userTaskContainer = document.querySelector('#userTaskContainer')
-
-    //create task form
+    //Function to create task form
     let createInputForm = () => {
         const addTaskFormContainer = document.createElement('form')
         addTaskFormContainer.setAttribute('id', 'addTaskFormContainer')
@@ -61,21 +58,97 @@ let toDoList = (() => {
         addTaskFormContainer.appendChild(addTaskTopBox)
         addTaskFormContainer.appendChild(addTaskBottomRow)
 
-        return addTaskFormContainer
+        const getUserData = () => {
+            getUserDescription = userDescriptionArea.value
+            getUserDueDate = datePicker.value
+            getProjectName = projectName.value
+            return{
+                getUserDescription,
+                getUserDueDate,
+                getProjectName
+            }
+        }
+        const pushFormDataToArray = (dataObject) => {
+            taskArray.push(dataObject)
+        }
+        
+        const clearFormData = () => {
+            userDescriptionArea.value = ""
+            datePicker.value = ""
+            projectName.value = ""
+        }
+        
+        checkmark.addEventListener('click', clickSubmit)
+        function clickSubmit(e) {
+            let userData = getUserData()
+            pushFormDataToArray(userData);
+            clearFormData();
+            let container = document.querySelector('#userTaskContainer')
+            container.removeChild(container.lastElementChild)
+            addAddTaskToDocument('userTaskContainer')
+            console.log(taskArray)
+        }
+
+        cancel.addEventListener('click', clickCancel)
+        function clickCancel() {
+            let container = document.querySelector('#userTaskContainer')
+            container.removeChild(container.lastElementChild)
+            addAddTaskToDocument('userTaskContainer')
+        }
+        return {
+            addTaskFormContainer,
+            getUserData,
+        }
     }
-    
-    
 
+    let createAddTask = () => {
+        const addTaskButton = document.createElement('li')
+        addTaskButton.setAttribute('class', "addTaskButton")
 
-    // console.log(createInputForm())
+        const addIcon = document.createElement('img')
+        addIcon.src = "images/add.svg"
 
-    return {taskFactoryFunc, taskArray, createInputForm}
+        const description = document.createElement('div')
+        description.setAttribute('class', 'taskDescription')
+        description.innerHTML = "Add Task"
+
+        addTaskButton.appendChild(addIcon)
+        addTaskButton.appendChild(description)
+
+        addTaskButton.addEventListener('click', clickAdd)
+        function clickAdd() {
+            let container = document.querySelector('#userTaskContainer')
+            container.removeChild(container.lastElementChild)
+            addFormToDocument("userTaskContainer");
+            
+            //activate bootstrap datepicker once form is created
+            $('#dueDate').datepicker({
+                todayBtn: "linked",
+                keyboardNavigation: false,
+                todayHighlight: true
+            });
+        }
+
+        return {
+            addTaskButton,
+        }
+    }
+
+    //Function to add task form onto an element
+    let addFormToDocument = (elementToAppendOnto) => {
+        let container = document.querySelector(`#${elementToAppendOnto}`)
+        let form = createInputForm();
+        container.appendChild(form.addTaskFormContainer)
+    }
+
+    let addAddTaskToDocument = (elementToAppendOnto) => {
+        let container = document.querySelector(`#${elementToAppendOnto}`)
+        let form = createAddTask();
+        container.appendChild(form.addTaskButton)
+    }
+
+    addAddTaskToDocument('userTaskContainer')
+        
+    return {taskFactoryFunc, taskArray}
 })();
 
-let userTaskContainer = document.querySelector('#userTaskContainer')
-console.log(userTaskContainer)
-
-let testInputForm = toDoList.createInputForm();
-console.log(testInputForm)
-
-userTaskContainer.appendChild(testInputForm)
